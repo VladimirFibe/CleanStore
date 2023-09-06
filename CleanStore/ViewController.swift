213ca7b77bc2731dbea48a9c3ed70e35f172c1ac
Vladimir Fibe <motiw@icloud.com>
2023-09-06 16:28:10 +0600

    Start

--- CleanStore/ViewController.swift
+++ CleanStore/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  CleanStore
-//
-//  Created by Vladimir Fibe on 06.09.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
